352cb03fa6d2a4eb785dc786148ad49af518ffaa
AZWAR HALIMU <azwarhalimu@gmail.com>
2025-07-14 20:50:15 +0800

    refactor: standardize string quotes and improve code readability in ItemSide component

--- src/components/layouts/Appshell/ItemSide.tsx
+++ src/components/layouts/Appshell/ItemSide.tsx
@@ -1,129 +1,131 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
 import {
   MdOutlineDashboard,
   MdOutlineAssessment,
-  MdOutlineManageAccounts,
-} from "react-icons/md";
-import { CiDatabase } from "react-icons/ci";
-import { TbReport } from "react-icons/tb";
-import Sidebar, { SidebarItem } from "./Sidebar";
+  MdOutlineManageAccounts
+} from 'react-icons/md'
+import { CiDatabase } from 'react-icons/ci'
+import { TbReport } from 'react-icons/tb'
+import Sidebar, { SidebarItem } from './Sidebar'
 
 type ItemProps = {
-  isSidebarOpen: boolean; // Pastikan `isSidebarOpen` bertipe boolean
-};
+  isSidebarOpen: boolean // Pastikan `isSidebarOpen` bertipe boolean
+}
 const Item: React.FC<ItemProps> = ({ isSidebarOpen }) => {
   const url =
-    typeof window != "undefined" ? window.location.href.split("/") : "rtt";
+    typeof window != 'undefined' ? window.location.href.split('/') : 'rtt'
 
-  const [menu, setMenu] = useState("");
-  const [user, setUser] = useState<string>("");
+  const [menu, setMenu] = useState('')
+  const [user, setUser] = useState<string>('')
 
   useEffect(() => {
-    if (typeof url[3] != "undefined") {
-      setMenu(url[3]);
-      const userData = localStorage.getItem("user");
-      setUser(userData || "");
+    if (typeof url[3] != 'undefined') {
+      setMenu(url[3])
     }
-  }, [url]);
+  }, [url])
+  useEffect(() => {
+    const userData = localStorage.getItem('user')
+    setUser(userData || '')
+  }, [url])
   return (
     <div
       className={`fixed w-64 h-full bg-[#222d32] shadow-md ${
-        isSidebarOpen ? "block" : "hidden"
+        isSidebarOpen ? 'block' : 'hidden'
       }`}
     >
-      {user == "user" && (
+      {user == 'user' && (
         <Sidebar isSidebarOpen>
           <SidebarItem
-            href="/dashboard-user"
+            href='/dashboard-user'
             icon={<MdOutlineDashboard size={22} />}
-            text="Dashboard"
-            active={menu === "dashboard" && true}
+            text='Dashboard'
+            active={menu === 'dashboard' && true}
           />
           <SidebarItem
-            href="/domain-user"
+            href='/domain-user'
             icon={<MdOutlineDashboard size={22} />}
-            text="Domain"
-            active={menu === "domain" && true}
+            text='Domain'
+            active={menu === 'domain' && true}
           />
           <SidebarItem
-            href="/aspek-user"
+            href='/aspek-user'
             icon={<CiDatabase size={22} />}
-            text="Aspek"
-            active={menu === "aspek" && true}
+            text='Aspek'
+            active={menu === 'aspek' && true}
           />
           <SidebarItem
-            href="/indikator-user"
+            href='/indikator-user'
             icon={<CiDatabase size={22} />}
-            text="Indikator"
-            active={menu === "indikator" && true}
+            text='Indikator'
+            active={menu === 'indikator' && true}
           />
           <SidebarItem
-            href="/cobit"
+            href='/cobit'
             icon={<CiDatabase size={22} />}
-            text="COBIT 5"
-            active={menu === "cobit" && true}
+            text={'COBIT 5'}
+            active={menu === 'cobit' && true}
           />
           <SidebarItem
-            href="/penilaian"
+            href='/penilaian'
             icon={<MdOutlineAssessment size={22} />}
-            text="Laporan Penilaian"
-            active={menu === "penilaian" && true}
+            text='Laporan Penilaian'
+            active={menu === 'penilaian' && true}
           />
         </Sidebar>
       )}
 
-      {user == "admin" && (
+      {user == 'admin' && (
         <Sidebar isSidebarOpen>
           <SidebarItem
-            href="/dashboard"
+            href='/dashboard'
             icon={<MdOutlineDashboard size={22} />}
-            text="Dashboard"
-            active={menu === "dashboard" && true}
+            text='Dashboard'
+            active={menu === 'dashboard' && true}
           />
           <SidebarItem
-            href="/domain"
+            href='/domain'
             icon={<MdOutlineDashboard size={22} />}
-            text="Domain"
-            active={menu === "domain" && true}
+            text='Domain'
+            active={menu === 'domain' && true}
           />
           <SidebarItem
-            href="/aspek"
+            href='/aspek'
             icon={<CiDatabase size={22} />}
-            text="Aspek"
-            active={menu === "aspek" && true}
+            text='Aspek'
+            active={menu === 'aspek' && true}
           />
           <SidebarItem
-            href="/indikator"
+            href='/indikator'
             icon={<CiDatabase size={22} />}
-            text="Indikator"
-            active={menu === "indikator" && true}
+            text='Indikator'
+            active={menu === 'indikator' && true}
           />
           <SidebarItem
-            href="/cobit"
+            href='/cobit'
             icon={<CiDatabase size={22} />}
-            text="COBIT 5"
-            active={menu === "cobit" && true}
+            text='COBIT 5'
+            active={menu === 'cobit' && true}
           />
           <SidebarItem
-            href="/penilaian"
+            href='/penilaian'
             icon={<MdOutlineAssessment size={22} />}
-            text="Laporan Penilaian"
-            active={menu === "penilaian" && true}
+            text='Laporan Penilaian'
+            active={menu === 'penilaian' && true}
           />
           <SidebarItem
-            href="/user"
+            href='/user'
             icon={<MdOutlineAssessment size={22} />}
-            text="User"
-            active={menu === "user" && true}
+            text='User'
+            active={menu === 'user' && true}
           />
         </Sidebar>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Item;
+export default Item
 
 {
   /* <div
